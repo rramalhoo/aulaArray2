f843efce7095cb33091dff1dc65cd46642425e80
rramalhoo <raphael.athenesi@gmail.com>
2024-10-02 09:02:43 -0300

    Aula2

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// const frutas=['Banana','Laranja','Maçã','Manga'];
+// text='<ul>'
+// for (i=0;i<frutas.length;i++){
+//      text+=`<li>${frutas[i]}</li>`;
+// }
+// text+='</ul>'
+// document.getElementById('saida').innerHTML=text;
+
+
+
+
+// const frutas=['Banana','Laranja','Maçã','Manga'];
+// text='<ul>'
+// frutas.forEach(minhaFuncao);
+// text+='</ul>'
+// document.getElementById('saida').innerHTML=text;
+//     function minhaFuncao(value){
+// text+=`<li>${value}</li>`   
+// }
+// document.getElementById('saida').innerHTML=text
+
+
+
+
+// const frutas = ['Banana', 'Laranja', 'Maçã', 'Manga'];
+// const listaFrutas = frutas.map(minhaFuncao).join('');
+// const  text = `<ul>${listaFrutas}</ul>`;
+// document.getElementById('saida').innerHTML = text;
+
+// function minhaFuncao(value) {
+//     return `<li>${value}</li>`
+// }
